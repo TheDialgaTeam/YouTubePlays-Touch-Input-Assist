0cf5701668882895bbdb9a751cd717ddbb7d926a
Yong Jian Ming <jianming1993@gmail.com>
2020-05-11 01:48:55 +0800

    Proper minify

--- youtubeplays-touchinputassist-beta.user.meta.js
+++ youtubeplays-touchinputassist-beta.user.meta.js
@@ -9,6 +9,8 @@
 // @downloadURL  https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist/raw/master/youtubeplays-touchinputassist-beta.user.js
 // @supportURL   https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist/issues
 // @match        https://www.youtube.com/watch?v=ArvVyvjm0yo*
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://kit.fontawesome.com/e5e217aee3.js
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_notification
