bb5a6218332d5f2b7099866f63724c296ecda809
Yong Jian Ming <jianming1993@gmail.com>
2021-01-23 21:22:55 +0800

    Update youtubeplays-touchinputassist.user.meta.js

--- youtubeplays-touchinputassist.user.meta.js
+++ youtubeplays-touchinputassist.user.meta.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         YouTubePlays Touch Input Assist
 // @namespace    TheDialgaTeam/YouTubePlays-Touch-Input-Assist
-// @version      0.2.5
+// @version      0.2.6
 // @author       Yong Jian Ming (jianmingyong)
 // @description  Generate useful touch commands aims to help new players to understand how the touch commands work.
 // @homepage     https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist
 // @updateURL    https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist/raw/master/youtubeplays-touchinputassist.user.meta.js
 // @downloadURL  https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist/raw/master/youtubeplays-touchinputassist.user.js
 // @supportURL   https://github.com/TheDialgaTeam/YouTubePlays-Touch-Input-Assist/issues
-// @match        https://www.youtube.com/watch?v=HaTczl7e5WA*
-// @match        https://www.youtube.com/watch?v=SPVynCS55Pc*
+// @match        https://www.youtube.com/watch?v=*
+// @match        https://www.youtube.com/watch?v=*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // @require      https://kit.fontawesome.com/e5e217aee3.js
 // @grant        GM_setValue
